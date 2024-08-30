9011854f6296b960f7fb4523bf07d28be2c83b7c
AdmiralPhantom <phantompuppetgaming@gmail.com>
2024-08-30 19:38:39 +0300

    CP

---
+++ Conditional Statements in JavaScript/script.js
@@ -0,0 +1,28 @@
+let age = 18;
+if (age < 18){
+        document.write("You cannot vote <br>");
+
+}
+else {
+    document.write("You can vote<br>")
+}
+// Even Odd
+let num = 12;
+if (num % 2 == 0){
+    document.write("<br> number is even");
+}else {
+    document.write ("<br> number is odd");
+}
+// if else if
+let per = 75;
+if (per >= 90){
+    document.write("<br> grade A");
+}else if (per >= 80){
+    document.write ("<br>Grade B");
+}else if (per >= 60){
+    document.write ("<br> Grade C");
+}else if (per >= 40){
+        document.write ("<br> Grade D");
+}else{
+    document.write ("<br> Grade F");
+}
