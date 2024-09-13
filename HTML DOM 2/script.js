1d8119c0f1496892ba89066606e9386b3811567b
AdmiralPhantom <phantompuppetgaming@gmail.com>
2024-09-13 19:46:52 +0300

    DOM

---
+++ HTML DOM 2/script.js
@@ -0,0 +1,7 @@
+document.getElementById("para1").innerHTML = "this is a modified paragraph";
+
+document.getElementsByClassName("headings").item(2).innerHTML = "this is a heading";
+
+document.getElementsByTagName("div")[0].innerHTML = "this is a modified division";
+
+document.getElementsByTagName("div")[0].style.backgroundColor="crimson";
